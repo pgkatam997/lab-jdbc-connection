26b75e5861de3a244272a66d7b682cb2a294e511
Purnima G Katam (S-59) <68459602+pgkatam997@users.noreply.github.com>
2020-08-17 18:54:46 +0530

    FROUF

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,21 @@
 package controller;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws ClassNotFoundException, SQLException {
 		
 	// Fill your code
-		
+		Connection con;
+		con=ConnectionManager.getConnection();
+		if(con!=null) {
+			System.out.println("Connection established");
+		}
+		else {
+			System.out.println("check your connection");
+		}
 	}
 }
